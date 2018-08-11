35630c23e1b15d22998e260203bae9b1bad0e74b
Alyssa Thunberg <alyssa.thunberg@gmail.com>
2018-08-11 05:48:19 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,11 @@
 def fizzbuzz(int)
   if int % 3 == 0
     "Fizz"
-  end
-  if int % 5 == 0
+  elseif
+  int % 5 == 0
     "Buzz"
+  else
+    int % 3 == 0 && int % 5 == 0
+    "FizzBuzz"
   end
 end
