285e631f1c8902f2f2bb88da70f762074cd00ba3
Alyssa Thunberg <alyssa.thunberg@gmail.com>
2018-08-11 05:59:31 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,7 @@
 def fizzbuzz(int)
-  if int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
-  elsif int % 3 == 0
+  if int % 3 == 0
     puts "Fizz"
   else int % 5 == 0
     puts "Buzz"
+  else
 end
